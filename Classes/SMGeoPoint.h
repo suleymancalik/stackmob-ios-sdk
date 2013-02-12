7a88059a0c252b14daa388130bb0ebe7e5c9185e
atupem <carl@atupem.com>
2013-02-12 11:26:57 -0800

    Add class descriptions

--- Classes/SMGeoPoint.h
+++ Classes/SMGeoPoint.h
@@ -1,5 +1,5 @@
 /*
- * Copyright 2012 StackMob
+ * Copyright 2012-2013 StackMob
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -18,7 +18,10 @@
 #import "SMResponseBlocks.h"
 #import "SMRequestOptions.h"
 
-// Category for NSDictionary
+
+/**
+ This category provides helper methods to get the latitude and longitude from a GeoPoint dictionary. 
+ */
 @interface NSDictionary (GeoPoint)
 
 - (NSNumber *)latitude;
@@ -26,12 +29,70 @@
 
 @end
 
+/**
+ `SMGeoPoint` is a subclass of NSDictionary, with helper methods to build GeoPoint dictionaries that are specific to the StackMob API.
+ */
+
 @interface SMGeoPoint : NSDictionary
 
+/**
+ Initializes an `SMGeoPoint` with the latitude and longitude provided.
+ 
+ @param latitude The latitude, represented as an `NSNumber`.
+ @param longitude The longitude, represented as an `NSNumber`.
+ 
+ @return An `SMGeoPoint`, for use as an attribute or as part of a query.
+ */
+
 + (SMGeoPoint *)geoPointWithLatitude:(NSNumber *)latitude Longitude:(NSNumber *)longitude;
+
+/**
+ Initializes an `SMGeoPoint` with the `CLLocationCoordinate2D` provided
+ 
+ @param coordinate The `CLLocationCoordinate2D` coordinate
+ 
+ @return An `SMGeoPoint`, for use as an attribute or as part of a query.
+ */
 + (SMGeoPoint *)geoPointWithCoordinate:(CLLocationCoordinate2D)coordinate;
 
+
+/**
+ Initializes an `SMGeoPoint` with coordinates provided by `SMLocationManager`
+ 
+ @param successBlock <i>typedef void (^SMGeoPointSuccessBlock)(SMGeoPoint *geoPoint)</i>. A block object to execute upon success.
+ @param failureBlock <i>typedef void (^SMFailureBlock)(NSError *error)</i>. A block object to execute upon failure.
+ 
+ @return An `SMGeoPoint`, for use as an attribute or as part of a query.
+ */
 + (void)getGeoPointForCurrentLocationOnSuccess:(SMGeoPointSuccessBlock)successBlock onFailure:(SMFailureBlock) failureBlock;
+
+/**
+ Initializes an `SMGeoPoint` with coordinates provided by `SMLocationManager`
+ 
+ @param options An options object that contains configurations for this request.
+ @param successBlock <i>typedef void (^SMGeoPointSuccessBlock)(SMGeoPoint *geoPoint)</i>. A block object to execute upon success.
+ @param failureBlock <i>typedef void (^SMFailureBlock)(NSError *error)</i>. A block object to execute upon failure.
+ 
+ @return An `SMGeoPoint`, for use as an attribute or as part of a query.
+ */
++ (void)getGeoPointForCurrentLocationWithOptions:(SMRequestOptions *)options
+                                       onSuccess:(SMGeoPointSuccessBlock)successBlock
+                                       onFailure:(SMFailureBlock)failureBlock;
+
+/**
+ Initializes an `SMGeoPoint` with coordinates provided by `SMLocationManager`
+ 
+ @param options An options object that contains configurations for this request.
+ @param successCallbackQueue The dispatch queue used to execute the success block. If nil is passed, the main queue is used.
+ @param failureCallbackQueue The dispatch queue used to execute the failure block. If nil is passed, the main queue is used.
+ @param successBlock <i>typedef void (^SMGeoPointSuccessBlock)(SMGeoPoint *geoPoint)</i>. A block object to execute upon success.
+ @param failureBlock <i>typedef void (^SMFailureBlock)(NSError *error)</i>. A block object to execute upon failure.
+ 
+ @return An `SMGeoPoint`, for use as an attribute or as part of a query.
+ */
++ (void)getGeoPointForCurrentLocationWithOptions:(SMRequestOptions *)options successCallbackQueue:(dispatch_queue_t)successCallbackQueue failureCallbackQueue:(dispatch_queue_t)failureCallbackQueue onSuccess:(SMGeoPointSuccessBlock)successBlock onFailure:(SMFailureBlock)failureBlock;
+
+
 
 @end
 
--- Classes/SMLocationManager.h
+++ Classes/SMLocationManager.h
@@ -1,5 +1,5 @@
 /*
- * Copyright 2012 StackMob
+ * Copyright 2012-2013 StackMob
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -17,17 +17,26 @@
 #import <Foundation/Foundation.h>
 #import <CoreLocation/CoreLocation.h>
 
-@class SMLocationManager;
-@protocol SMLocationManagerDelegate <NSObject>
-@required
--(void)SM_locationManagerDidFailWithError:(NSError *)error;
-@end
-
-
-
+/**
+ `SMLocationManager` is a CLLocationManager singleton 
+ 
+ @note You shouldn't have to access SMLocationManager directly. It's recommended that you subclass SMLocationManager to add more functionality.
+ 
+ ## References ##
+ 
+ [Apple's CLLocationManager class reference](https://developer.apple.com/library/mac/#documentation/CoreLocation/Reference/CLLocationManager_Class/CLLocationManager/CLLocationManager.html )
+ */
 @interface SMLocationManager : NSObject <CLLocationManagerDelegate>
 
+/**
+ locationManager is the CLLocationManager this singleton uses to recieve updates
+ */
+
 @property (nonatomic, strong) CLLocationManager* locationManager;
+
+/**
+ locationManagerError is a property to store errors that the CLLocationManager returns
+*/
 @property (nonatomic, strong) NSError *locationManagerError;
 
 + (SMLocationManager *)sharedInstance;
