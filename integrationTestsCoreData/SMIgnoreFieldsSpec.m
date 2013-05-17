285a02a3fb5886fe805d7c4f2c6bfb8ae1d12577
Matt Vaznaian <matt@stackmob.com>
2013-05-16 19:42:21 -0700

    Add test skeleton

---
+++ integrationTestsCoreData/SMIgnoreFieldsSpec.m
@@ -0,0 +1,39 @@
+/*
+ * Copyright 2012-2013 StackMob
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#import <Kiwi/Kiwi.h>
+#import "StackMob.h"
+#import "SMTestProperties.h"
+
+SPEC_BEGIN(SMIgnoreFieldsSpec)
+
+describe(@"Writing objects with ignored fields", ^{
+    __block SMTestProperties *testProperties = nil;
+    beforeEach(^{
+        SM_CACHE_ENABLED = YES;
+        testProperties = [[SMTestProperties alloc] init];
+        [testProperties.client setUserSchema:@"User3"];
+    });
+    afterEach(^{
+        SM_CACHE_ENABLED = NO;
+        //SM_CORE_DATA_DEBUG = NO;
+    });
+    it(@"saves", ^{
+        
+    });
+});
+
+SPEC_END
